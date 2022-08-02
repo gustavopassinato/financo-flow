b0e0ca7b8033d0d25b0da06d321886a7ac2c1572
Gustavo Passinato Neves <passinato11@gmail.com>
2022-08-01 21:09:19 -0300

    First commit

---
+++ src/main/java/br/com/financo/flow/FinancoFlowApplication.java
@@ -0,0 +1,13 @@
+package br.com.financo.flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancoFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancoFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/financo/flow/FinancoFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.financo.flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancoFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
